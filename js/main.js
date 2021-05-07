b9e9d6ce4fba15bbb48f657837242cbb7dca695a
unosanta <santahouse0311@sina.com>
2021-05-07 21:57:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,35 +4,6 @@ sitemap:
 exclude: 'yes'
 ---
 
-var iUp = (function () {
-  var t = 0,
-    d = 150,
-    clean = function () {
-      t = 0;
-    },
-    up = function (e) {
-      setTimeout(function () {
-        $(e).addClass("up")
-      }, t);
-      t += d;
-    },
-    down = function (e) {
-      $(e).removeClass("up");
-    },
-    toggle = function (e) {
-      setTimeout(function () {
-        $(e).toggleClass("up")
-      }, t);
-      t += d;
-    };
-  return {
-    clean: clean,
-    up: up,
-    down: down,
-    toggle: toggle
-  }
-})();
-
 $(document).ready(function () {
   {% if site.disable_landing_page != true %}
   $('a.blog-button').click(function (e) {
@@ -59,26 +30,30 @@ $(document).ready(function () {
   {% endif %}
 
 
-  $('.btn-mobile-menu__icon').click(function() {
-      if ($('.navigation-wrapper').css('display') == "block") {
-        $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-          $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-          $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-        });
-        $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+  // $('.btn-mobile-menu__icon').click(function() {
+  //     if ($('.navigation-wrapper').css('display') == "block") {
+  //       $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+  //         $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+  //         $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+  //       });
+  //       $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
 
-      } else {
-        $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-      }
-      $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
-  });
+  //     } else {
+  //       $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+  //     }
+  //     $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
+  // });
 
-  $(".iUp").each(function (i, e) {
-    iUp.up(e);
-  });
+  
+  $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn')
+  })
 
-  $(".js-avatar")[0].onload = function () {
-   $(".js-avatar").addClass("show");
-  }
+  $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn')
+  })
+
 
 })
