67aef37ee2952c118a1ca93110ff76bbd749bfef
unosanta <santahouse0311@sina.com>
2021-05-07 21:00:47 +0800

    Improve home

--- js/home.js
+++ js/home.js
@@ -44,18 +44,3 @@ $(document).ready(function () {
   }
 
 });
-
-$('.btn-mobile-menu__icon').click(function() {
-  if ($('.navigation-wrapper').css('display') == "block") {
-    $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-      $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-    });
-    $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-
-  } else {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-  }
-  $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
-});
-
--- js/main.js
+++ js/main.js
@@ -48,11 +48,13 @@ $(document).ready(function () {
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.panel-cover').addClass('panel-cover--collapsed')    
+    $('.content-wrapper__inner').css('display', 'block')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.content-wrapper__inner').css('display', 'block')
   }
   {% endif %}
 
